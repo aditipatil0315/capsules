178b595cb80a4aed814dea0426b9a7e036676764
Aditi Patil <aditiprakashpatil7@gmail.com>
2025-10-01 22:50:30 +0530

    main and counter changes

--- src/counter.js
+++ src/counter.js
@@ -1,13 +1,93 @@
+// import gsap from "gsap";
+
+// export function setupMarqueeAnimation() {
+//   const marqueeItems = gsap.utils.toArray(".marquee h1");
+//   if (marqueeItems.length > 0) {
+//     const tl = horizontalLoop(marqueeItems, {
+//       repeat: -1,
+//       paddingRight: 30,
+//     });
+//   }
+// }
+
+// function horizontalLoop(items, config) {
+//   items = gsap.utils.toArray(items);
+//   config = config || {};
+//   let tl = gsap.timeline({
+//     repeat: config.repeat,
+//     defaults: { ease: "none" },
+//   });
+//   let length = items.length;
+//   let startX = items[0].offsetLeft;
+//   let widths = [];
+//   let xPercents = [];
+//   let pixelsPerSecond = (config.speed || 1) * 100;
+//   let totalWidth, curX, distanceToStart, distanceToLoop, i;
+
+//   gsap.set(items, {
+//     xPercent: (i, el) => {
+//       let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
+//       xPercents[i] =
+//         (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 +
+//         gsap.getProperty(el, "xPercent");
+//       return xPercents[i];
+//     },
+//   });
+
+//   gsap.set(items, { x: 0 });
+//   totalWidth =
+//     items[length - 1].offsetLeft +
+//     (xPercents[length - 1] / 100) * widths[length - 1] -
+//     startX +
+//     items[length - 1].offsetWidth *
+//       gsap.getProperty(items[length - 1], "scaleX") +
+//     (parseFloat(config.paddingRight) || 0);
+
+//   for (i = 0; i < length; i++) {
+//     let item = items[i];
+//     curX = (xPercents[i] / 100) * widths[i];
+//     distanceToStart = item.offsetLeft + curX - startX;
+//     distanceToLoop =
+//       distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+//     tl.to(
+//       item,
+//       {
+//         xPercent: ((curX - distanceToLoop) / widths[i]) * 100,
+//         duration: distanceToLoop / pixelsPerSecond,
+//       },
+//       0
+//     ).fromTo(
+//       item,
+//       { xPercent: ((curX - distanceToLoop + totalWidth) / widths[i]) * 100 },
+//       {
+//         xPercent: xPercents[i],
+//         duration:
+//           (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
+//         immediateRender: false,
+//       },
+//       distanceToLoop / pixelsPerSecond
+//     );
+//   }
+
+//   tl.progress(1, true).progress(0, true);
+//   return tl;
+// }
+
+
 import gsap from "gsap";
 
 export function setupMarqueeAnimation() {
-  const marqueeItems = gsap.utils.toArray(".marquee h1");
-  if (marqueeItems.length > 0) {
-    const tl = horizontalLoop(marqueeItems, {
-      repeat: -1,
-      paddingRight: 30,
-    });
-  }
+  // run only after window load to ensure widths are correct
+  window.addEventListener("load", () => {
+    const marqueeItems = gsap.utils.toArray(".marquee h1");
+    if (marqueeItems.length > 0) {
+      horizontalLoop(marqueeItems, {
+        repeat: -1,
+        paddingRight: 30,
+        speed: 2.5, // increase for faster marquee
+      });
+    }
+  });
 }
 
 function horizontalLoop(items, config) {
@@ -39,8 +119,7 @@ function horizontalLoop(items, config) {
     items[length - 1].offsetLeft +
     (xPercents[length - 1] / 100) * widths[length - 1] -
     startX +
-    items[length - 1].offsetWidth *
-      gsap.getProperty(items[length - 1], "scaleX") +
+    items[length - 1].offsetWidth * gsap.getProperty(items[length - 1], "scaleX") +
     (parseFloat(config.paddingRight) || 0);
 
   for (i = 0; i < length; i++) {
@@ -61,8 +140,7 @@ function horizontalLoop(items, config) {
       { xPercent: ((curX - distanceToLoop + totalWidth) / widths[i]) * 100 },
       {
         xPercent: xPercents[i],
-        duration:
-          (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
+        duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
         immediateRender: false,
       },
       distanceToLoop / pixelsPerSecond
@@ -72,3 +150,4 @@ function horizontalLoop(items, config) {
   tl.progress(1, true).progress(0, true);
   return tl;
 }
+
--- src/main.js
+++ src/main.js
@@ -1,162 +1,221 @@
+// src/main.js (REPLACEMENT)
 import { setupMarqueeAnimation } from "./counter.js";
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
-import Lenis from '@studio-freight/lenis'
+import Lenis from "@studio-freight/lenis";
 
-// import Lenis from "lenis";
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 document.addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(ScrollTrigger, SplitText);
-
   const lenis = new Lenis();
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
   lenis.on("scroll", ScrollTrigger.update);
-  gsap.ticker.add((time) => lenis.raf(time * 1000));
-  gsap.ticker.lagSmoothing(0);
+  window.addEventListener("load", () => {
+    ScrollTrigger.refresh();
+  });
 
   const cards = gsap.utils.toArray(".card");
+  if (!cards || cards.length === 0) return;
+
   const introCard = cards[0];
-  
+  if (!introCard) return;
+
+  // Split titles into chars
   const titles = gsap.utils.toArray(".card-title h1");
-    titles.forEach((title) => {
+  titles.forEach((title) => {
     const split = new SplitText(title, {
-    type: "char",
-    charsClass: "char",
-    tag: "div",
+      type: "char",
+      charsClass: "char",
+      tag: "div",
+    });
+
+    split.chars.forEach((char) => {
+      char.innerHTML = `<span aria-hidden="true">${char.textContent}</span>`;
+    });
   });
 
-  split.chars.forEach((char) => {
-    char.innerHTML = `<span>${char.textContent}</span>`;
-  });
-});
+  const cardImgWrapper = introCard.querySelector(".card-img");
+  const cardImg = introCard.querySelector(".card-img img");
+  if (cardImgWrapper) gsap.set(cardImgWrapper, { scale: 0.5, borderRadius: "400px" });
+  if (cardImg) gsap.set(cardImg, { scale: 1.5 });
 
 
-const cardImgWrapper = introCard.querySelector(".card-img");
-const cardImg = introCard.querySelector(".card-img img");
-gsap.set(cardImgWrapper, {scale: 0.5, borderRadius: "400px"});
-gsap.set(cardImg, {scale: 1.5});
+  function animateContentIn(titleChars, description) {
+    if (titleChars && titleChars.length !== 0) {
+      gsap.to(titleChars, { x: "0%", duration: 0.75, ease: "power4.out", stagger: 0.02 });
+    }
+    if (description) {
+      gsap.to(description, {
+        x: 0,
+        opacity: 1,
+        duration: 0.75,
+        delay: 0.1,
+        ease: "power4.out",
+      });
+    }
+  }
 
-function animateContentIn(titleChars, description) {
-  gsap.to(titleChars, { x: "0%", duration: 0.75, ease: "power4.out" });
-  gsap.to(description, {
-    x: 0,
-    opacity: 1,
-    duration: 0.75,
-    delay: 0.1,
-    ease: "power4.out",
-  });
-}
+  function animateContentOut(titleChars, description) {
 
-const marquee = introCard.querySelector(".card-marquee .marquee");
-const titleChars = introCard.querySelector(".char span");
-const description = introCard.querySelector(".card-description");
+    if (titleChars && titleChars.length !== 0) {
+      gsap.to(titleChars, { x: "-100%", duration: 0.6, ease: "power4.in", stagger: 0.01 });
+    }
+    if (description) {
+      gsap.to(description, { x: 20, opacity: 0, duration: 0.6, ease: "power4.in" });
+    }
+  }
 
-ScrollTrigger.create({
+
+  const marquee = introCard.querySelector(".card-marquee .marquee");
+  const titleChars = introCard.querySelectorAll(".card-title .char span"); // NodeList
+  const description = introCard.querySelector(".card-description");
+
+
+  ScrollTrigger.create({
     trigger: introCard,
     start: "top top",
     end: "+=300vh",
     onUpdate: (self) => {
+      try {
         const progress = self.progress;
         const imgScale = 0.5 + progress * 0.5;
         const borderRadius = 400 - progress * 375;
         const innerImgScale = 1.5 - progress * 0.5;
 
-        gsap.set(cardImgWrapper, {
+        if (cardImgWrapper) {
+          gsap.set(cardImgWrapper, {
             scale: imgScale,
             borderRadius: borderRadius + "px",
-        });
-        gsap.set(cardImg, { scale: innerImgScale });
+          });
+        }
+        if (cardImg) gsap.set(cardImg, { scale: innerImgScale });
 
-        if (imgScale >= 0.5 && imgScale <= 0.75) {
+
+        if (marquee) {
+          if (imgScale >= 0.5 && imgScale <= 0.75) {
             const fadeProgress = (imgScale - 0.5) / (0.75 - 0.5);
             gsap.set(marquee, { opacity: 1 - fadeProgress });
-        } else if (imgScale < 0.5) {
+          } else if (imgScale < 0.5) {
             gsap.set(marquee, { opacity: 1 });
-        } else if (imgScale > 0.75) {
+          } else if (imgScale > 0.75) {
             gsap.set(marquee, { opacity: 0 });
+          }
         }
 
-        if (progress >= 1 && introCard.contentRevealed) {
+
+        if (progress >= 1 && !introCard.contentRevealed) {
           introCard.contentRevealed = true;
           animateContentIn(titleChars, description);
-      }
+        }
 
         if (progress < 1 && introCard.contentRevealed) {
           introCard.contentRevealed = false;
           animateContentOut(titleChars, description);
+        }
+      } catch (err) {
+
+        console.error("introCard ScrollTrigger error:", err);
       }
-        
+    },
+  });
 
 
-        },
-    });
-
-    cards.forEach((card, index) => {
+  cards.forEach((card, index) => {
     const isLastCard = index === cards.length - 1;
     ScrollTrigger.create({
-    trigger: card,
-    start: "top top",
-    end: isLastCard ? "+=100vh" : "top top",
-    endTrigger: isLastCard ? null : cards[cards.length - 1],
-    pin: true,
-    pinSpacing: isLastCard,
+      trigger: card,
+      start: "top top",
+      end: isLastCard ? "+=100vh" : "top top",
+      endTrigger: isLastCard ? null : cards[cards.length - 1],
+      pin: true,
+      pinSpacing: isLastCard,
     });
-
   });
+
 
   cards.forEach((card, index) => {
     if (index < cards.length - 1) {
-        const cardWrapper = card.querySelector(".card-wrapper");
-        ScrollTrigger.create({
-            trigger: cards[index + 1],
-            start: "top bottom",
-            end: "top top",
-            onUpdate: (self) => {
-                const progress = self.progress;
-                gsap.set(cardImgWrapper, {
-                    scale: 1 - progress * 0.25,
-                    opacity: 1 - progress,
-                });
-            },
-        });
+      const next = cards[index + 1];
+      ScrollTrigger.create({
+        trigger: next,
+        start: "top bottom",
+        end: "top top",
+        onUpdate: (self) => {
+          try {
+            const progress = self.progress;
+            if (cardImgWrapper) {
+
+              gsap.set(cardImgWrapper, {
+                scale: 1 - progress * 0.25,
+                opacity: 1 - progress,
+              });
+            }
+          } catch (err) {
+            console.error("img progress update error:", err);
+          }
+        },
+      });
     }
-});
+  });
 
-
-      cards.forEach((card, index) => {
-    if (index > 0) {
-        const cardImg = card.querySelector(".card-img img");
-        const imgContainer = card.querySelector(".card-img");
-        ScrollTrigger.create({
-            trigger: card,
-            start: "top bottom",
-            end: "top top",
-            onUpdate: (self) => {
-                const progress = self.progress;
-                gsap.set(cardImg, {scale: 2 - progress });
-                gsap.set(imgContainer,{borderRadius: 150 -progress * 125 + "px"});
-
-            },
-        });
-    }
-});
-
-
-      cards.forEach((card, index) => {
+  // per-card image scale / borderRadius transitions
+  cards.forEach((card, index) => {
     if (index === 0) return;
 
-    const cardDescription = card.querySelector(".card.description");
+    const cardImg = card.querySelector(".card-img img");
+    const imgContainer = card.querySelector(".card-img");
+    ScrollTrigger.create({
+      trigger: card,
+      start: "top bottom",
+      end: "top top",
+      onUpdate: (self) => {
+        try {
+          const progress = self.progress;
+          if (cardImg) gsap.set(cardImg, { scale: 2 - progress });
+          if (imgContainer) gsap.set(imgContainer, { borderRadius: 150 - progress * 125 + "px" });
+        } catch (err) {
+          console.error("card image update error:", err);
+        }
+      },
+    });
+  });
+
+  // animate title & description on entering each card
+  cards.forEach((card, index) => {
+    if (index === 0) return;
+
+    const cardDescription = card.querySelector(".card-description"); // fixed selector
     const cardTitleChars = card.querySelectorAll(".char span");
 
     ScrollTrigger.create({
-        trigger: card,
-        start: "top top",
-        onEnter: () => animateContentIn(cardTitleChars, cardDescription),
-        onLeaveBack: () => animateContentOut(cardTitleChars, cardDescription),
+      trigger: card,
+      start: "top top",
+      onEnter: () => {
+        try {
+          animateContentIn(cardTitleChars, cardDescription);
+        } catch (err) {
+          console.error("onEnter animate error:", err);
+        }
+      },
+      onLeaveBack: () => {
+        try {
+          animateContentOut(cardTitleChars, cardDescription);
+        } catch (err) {
+          console.error("onLeaveBack animate error:", err);
+        }
+      },
     });
-});
+  });
 
+  // finally setup marquee animation (in counter.js) and refresh ScrollTrigger
   setupMarqueeAnimation();
-  
+
+  // small safety: recalc after setup
+  ScrollTrigger.refresh();
 });
